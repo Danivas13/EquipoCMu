61fe288c244fd2fd023598d25ff6fc82ec025f65
Danivas13 <danielwebmas@gmail.com>
2021-09-07 23:46:43 -0500

    Prueba M

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -2,10 +2,11 @@
     "COMMONS": {
         "MENU": {
           "MNU_BDL_SCE":"Fromulario clientes",
-			"MNU_BDL_SCE1":"Buscar cliente",
+			"MNU_BDL_SCE11":"Buscar cliente",
 			"MNU_BDL_SCE2":"Listado clientes",
-			"MNU_BDL_SCE3":"Eliminar clientes"
-	    "MNU_BDL_DANIVAS":"Insertar Producto"
+			"MNU_BDL_SCE3":"Eliminar clientes",
+	    "MNU_BDL_DANIVAS":"Insertar Producto",
+	    "MNU_BDL_DANIVAS_P":"Visualizar Productos"
         }
     }
 }
