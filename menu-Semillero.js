3f89b25c57181075fb1f82c97bb06b761b1a33df
unknown <byrondanipm@gmail.com>
2021-09-07 21:52:37 -0500

    se agregaron los menus

---
+++ menu-Semillero.js
@@ -0,0 +1,10 @@
+{
+    "COMMONS": {
+        "MENU": {
+            "MNU_BDL_SCE":"Fromulario clientes",
+			"MNU_BDL_SCE1":"Buscar cliente",
+			"MNU_BDL_SCE2":"Listado clientes",
+			"MNU_BDL_SCE3":"Eliminar clientes"
+        }
+    }
+}
