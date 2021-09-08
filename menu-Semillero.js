b51ebec6b06eb2c67fdc2967c2e69a43955b5d2a
Daniel Vasquez <danielwebmas@gmail.com>
2021-09-07 23:49:34 -0500

    Update menu-Semillero.js

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -2,7 +2,7 @@
     "COMMONS": {
         "MENU": {
           "MNU_BDL_SCE":"Fromulario clientes",
-			"MNU_BDL_SCE11":"Buscar cliente",
+			"MNU_BDL_SCE11888":"Buscdfsdfsdfar cliente",
 			"MNU_BDL_SCE2":"Listado clientes",
 			"MNU_BDL_SCE3":"Eliminar clientes",
 	    "MNU_BDL_DANIVAS":"Insertar Producto",
