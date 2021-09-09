67d68df18af744be92863def27f402c9830f28c1
ByronDaniel <byrondanipm@gmail.com>
2021-09-09 12:03:02 -0500

    se agrego la entidad bddl y el formulario

---
+++ ProyectoTCM/MMUNS/SSMMM/forms/T_MMUNSFVHMRFSG_308/events/executeCommand/VA_VABUTTONBHMOUKX_842721.js
@@ -0,0 +1,11 @@
+
+
+//Entity: BDDLEntidad
+//BDDLEntidad. (Button) View: EstudianteBDDLForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONBHMOUKX_842721 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.BDDLEntidad = true;
+
+};
