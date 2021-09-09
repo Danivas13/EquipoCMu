0f2e693913fcf245fc26cdc5cc68014dfaad1b42
Jefferson-N2 <jmnorona@uce.edu.ec>
2021-09-09 01:19:28 -0500

    Agregando Entidad EN_JNMOYADWW_337

---
+++ ProyectoTCM/MMUNS/SSMMM/forms/T_MMUNSNHFJGGEJ_793/events/executeCommand/VA_VABUTTONHPNLWNG_219577.js
@@ -0,0 +1,12 @@
+
+
+//Entity: JNEntidad
+//JNEntidad. (Button) View: EstudianteJNForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONHPNLWNG_219577 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = false;
+    Console.log(entities.JNEntidad.nombre+" "+entities.JNEntidad.apellido);
+     executeCommandEventArgs.commons.messageHandler.showMessagesInformation("El nombre completo es: "+entities.JNEntidad.nombre+" "+entities.JNEntidad.apellido);
+
+};
