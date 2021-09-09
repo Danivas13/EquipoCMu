f3cee8d02c503bff30eeac01378f1caf4b697c0b
Danivas13 <danielwebmas@gmail.com>
2021-09-09 00:58:39 -0500

    agregado vs es_EC

--- ProyectoTCM/cwc/languages/menu-MMUNS.js
+++ ProyectoTCM/cwc/languages/menu-MMUNS.js
@@ -1,8 +1,8 @@
 {
     "COMMONS": {
         "MENU": {
-			"Fase 4 TCM":"Taller Git Fase 4 TCM",
-			"Estudiante DJVG":"Buscar Cliente BDL"
+			"Fase 4 TCM": "Taller Git Fase 4 TCM",
+			"Estudiante DJVG": "Buscar Cliente BDL"
         }
     }
 }
