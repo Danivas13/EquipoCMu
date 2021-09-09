764e4171f7a9182b689b033330ed47ad6fffe5a7
Erick <erick.santos.mb3i@gmail.com>
2021-09-09 15:20:59 -0500

    Creacion formulario EstudiantesESBMForm

---
+++ ProyectoTCM/MMUNS/SSMMM/forms/T_MMUNSTDHCDWWU_517/events/T_MMUNSTDHCDWWU_517.js
@@ -0,0 +1,1 @@
+//"TaskId": "T_MMUNSTDHCDWWU_517"
---
+++ ProyectoTCM/MMUNS/SSMMM/forms/T_MMUNSTDHCDWWU_517/events/executeCommand/VA_VABUTTONGIKEXBR_121382.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EBSMEntidad
+//EBSMEntidad. (Button) View: EstudianteEBSMForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONGIKEXBR_121382 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.EBSMEntidad = true;
+
+};
